8fc2b1489aec5ad6357194731275b8dcade919af
meguko2002 <meguko2002@gmail.com>
2020-04-24 17:38:14 +0900

    imageのx軸設定できた

--- bandingDesigner.py
+++ bandingDesigner.py
@@ -25,10 +25,10 @@ def tx(N):
 
 
 class Simuration ():
-    def __init__(self , file , dt , isimage=True):
+    def __init__(self , file , dt , ps=200, isimage=True):
         self.file = file
         self.dt = dt
-        self.ps = 200
+        self.ps = ps
         self.datanum = 0
         self.f_range = 100
         self.amp_max = 5
@@ -52,6 +52,7 @@ class Simuration ():
     def set_fig(self):
         self.fig , (self.ax_original_data , self.ax_ref_image , self.ax_sim_image , \
                     self.ax_wave , self.ax_fft) = plt.subplots ( nrows=5 , figsize=(8 , 8) )
+        self.fig.suptitle('Banding Designer', fontsize=16)
 
     def set_ax(self):
         self.line_refwave , = self.ax_wave.plot ( [] , [] , 'green' , \
@@ -84,47 +85,45 @@ class Simuration ():
         self.ax_fft.legend ()
 
     def set_x_scale(self, label):
-        self.ax_ref_image.set_xlabel (label)
-        self.ax_sim_image.set_xlabel (label)
-        # if label == 'sec':
-        #     delta, _ = self.tx(self.datanum * dt)
-        #     self.ax_ref_image.set_xticks(np.arange(0, self.datanum, delta / self.dt))
-        #     self.ax_ref_image.set_xticklabels(np.arange(0, self.datanum * dt,  delta))
-        #
-        # elif label == 'mm':
-        #     delta, unit_num = self.tx(self.datanum * dt * self.ps)
-        #     self.ax_ref_image.set_xticks(np.arange(0, self.datanum, int(delta /self.dt / self.ps)))
-        #     self.ax_ref_image.set_xticklabels(np.arange(0, self.datanum * dt * self.ps, delta))
-        #     print(np.arange(0, delta * unit_num,  delta))
-        # elif label == 'pixel':
-        #     delta, _ = self.tx(self.datanum)
-        #     self.ax_ref_image.set_xticks(np.arange(0, self.datanum, delta))
-        #     self.ax_ref_image.set_xticklabels(np.arange(0, self.datanum, delta))
-
-
         if label == 'pixel':
             max_data_num = self.datanum
-            delta , _ = tx ( max_data_num )
+            delta, _ = tx ( max_data_num )
             delta_pix = delta
+            """original_image_setting"""
+            org_max_data_num = self.original_datanum
+            org_delta, _ =tx (org_max_data_num)
+            org_delta_pix = org_delta
         elif label == 'sec':
-            max_data_num = self.datanum * dt
+            max_data_num = self.datanum * self.dt
             delta , _ = tx ( max_data_num )
-            delta_pix = int(delta/ self.dt)
+            delta_pix = int(delta / self.dt)
+            """original_image_setting"""
+            org_max_data_num = self.original_datanum * self.dt
+            org_delta, _ = tx ( org_max_data_num )
+            org_delta_pix = int(org_delta / self.dt)
         elif label == 'mm':
             max_data_num = self.datanum * dt *self.ps
             delta,_= tx (max_data_num)
             delta_pix = int ( delta / self.dt / self.ps )
+            """original_image_setting"""
+            org_max_data_num = self.original_datanum * self.dt * self.ps
+            org_delta , _ = tx ( org_max_data_num )
+            org_delta_pix = int(org_delta/ self.dt / self.ps)
+
         self.ax_ref_image.set_xticks(np.arange(0, self.datanum, delta_pix))
         self.ax_ref_image.set_xticklabels(np.round(np.arange(0, max_data_num, delta),1))
         self.ax_sim_image.set_xticks(np.arange(0, self.datanum, delta_pix))
         self.ax_sim_image.set_xticklabels(np.round(np.arange(0, max_data_num, delta), 1))
 
+        self.ax_original_data.set_xticks(np.round(np.arange(0, self.original_datanum, org_delta_pix),1))
+        self.ax_original_data.set_xticklabels(np.round(np.arange(0, org_max_data_num, org_delta), 1))
+
         self.fig.canvas.draw()
 
     def set_radio_box(self):
         axcolor = 'lightgoldenrodyellow'
-        ra_xs = plt.axes([0.8, 0.3, 0.1, 0.1], facecolor=axcolor)
-        self.radio_x_scale = RadioButtons(ra_xs, ('sec', 'mm', 'pixel'))
+        ra_xs = plt.axes([0.8, 0.9, 0.1, 0.1], facecolor=axcolor)
+        self.radio_x_scale = RadioButtons(ra_xs, ('pixel','mm','sec'))
 
     def onselect(self , eclick , erelease):
         pass  # 座標を返すこともできるが、on,offの座標が逆転するバグがあるので使わない
@@ -168,6 +167,7 @@ class Simuration ():
 
     def ref_setting(self):
         self.ax_original_data.cla ()
+        self.ax_original_data.set_title ( 'original image' )
         if self.isimage:    self.set_original_image()
         else:   self.set_original_wave()
         self.trim_original_data()
@@ -238,12 +238,14 @@ class Simuration ():
 
     def set_original_image(self):
         self.raw_image = cv2.imdecode ( np.fromfile ( self.file , np.uint8 ) , 0 )
+        self.original_datanum = len(self.raw_image)
         self.ax_original_data.imshow ( self.raw_image.T , 'gray' , vmin=0 , vmax=255 , aspect='auto' )
 
     def set_original_wave(self):
         self.original_wave = np.loadtxt ( 'sinwave.txt' , dtype='uint8' )
         self.org_timeobj = imageanalyzer2.Time ( wave=self.original_wave , dt=self.dt )
-        dot_sq = np.arange(0, self.org_timeobj.n)
+        self.original_datanum = self.org_timeobj.n
+        dot_sq = np.arange(0, self.original_datanum )
         self.line_origilal_wave , = self.ax_original_data.plot (dot_sq, self.org_timeobj.wave, 'black' , \
                                                                  linewidth=1 , label='origin' )
         self.ax_original_data.grid ( True )
@@ -271,5 +273,5 @@ if __name__ == '__main__':
     dt = 0.0005
     # file = 'sinwave.txt'
     file = 'ref.tif'
-    sim = Simuration ( file, dt , isimage=1)
+    sim = Simuration ( file, dt , ps=100, isimage=1)
     sim.run ()
